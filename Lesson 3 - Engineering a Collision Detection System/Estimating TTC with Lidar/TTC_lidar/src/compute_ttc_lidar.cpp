20e3632c90c873d58a726a1799cdd3f30394633c
Dobuzi <kimjw7981@gmail.com>
2021-10-03 06:30:05 +0000

    solve lidar ttc quiz

--- Lesson 3 - Engineering a Collision Detection System/Estimating TTC with Lidar/TTC_lidar/src/compute_ttc_lidar.cpp
+++ Lesson 3 - Engineering a Collision Detection System/Estimating TTC with Lidar/TTC_lidar/src/compute_ttc_lidar.cpp
@@ -19,12 +19,18 @@ void computeTTCLidar(std::vector<LidarPoint> &lidarPointsPrev,
     double minXPrev = 1e9, minXCurr = 1e9;
     for (auto it = lidarPointsPrev.begin(); it != lidarPointsPrev.end(); ++it)
     {
-        minXPrev = minXPrev > it->x ? it->x : minXPrev;
+        if (-1*laneWidth/2 <= it->y && it->y <= laneWidth/2)
+        {
+            minXPrev = minXPrev > it->x ? it->x : minXPrev;
+        }
     }
 
     for (auto it = lidarPointsCurr.begin(); it != lidarPointsCurr.end(); ++it)
     {
-        minXCurr = minXCurr > it->x ? it->x : minXCurr;
+        if (-1*laneWidth/2 <= it->y && it->y <= laneWidth/2)
+        {
+            minXCurr = minXCurr > it->x ? it->x : minXCurr;
+        }
     }
 
     // compute TTC from both measurements
