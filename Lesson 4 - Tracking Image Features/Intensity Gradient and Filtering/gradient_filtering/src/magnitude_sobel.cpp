c8ee9070708d9254999a917ba2b0ca089e60c978
Dobuzi <kimjw7981@gmail.com>
2021-10-04 14:29:44 +0000

    solve magnitude sobel quiz 2

--- Lesson 4 - Tracking Image Features/Intensity Gradient and Filtering/gradient_filtering/src/magnitude_sobel.cpp
+++ Lesson 4 - Tracking Image Features/Intensity Gradient and Filtering/gradient_filtering/src/magnitude_sobel.cpp
@@ -42,43 +42,56 @@ void magnitudeSobel()
 
     // compute magnitude image based on the equation presented in the lesson 
     // ToDo : Add your code here
-    float gradient, gradient_x, gradient_y, theta, norm_grad;
-    int delta = 1;
     
-    int nrows = imgBlur.rows-delta;
-    int ncols = imgBlur.cols-delta;
-    
-    float gradients[nrows][ncols];
-    
-    float max_grad = 0;
-    float min_grad = 255;
+    int nrows = imgBlur.rows;
+    int ncols = imgBlur.cols;
 
     cv::Mat magnitude;
     magnitude.create(nrows, ncols, CV_8UC1);
 
-    for (int i = 0; i < nrows; i++)
+    bool selfmade = true;
+
+    if (selfmade)
     {
-        for (int j = 0; j < ncols; j++)
+        float gradient, gradient_x, gradient_y, theta, norm_grad;
+        int delta = 1;
+    
+        float gradients[nrows-delta][ncols-delta];
+    
+        float max_grad = 0;
+        float min_grad = 255;
+
+        for (int i = 0; i < nrows-delta; i++)
         {
-            gradient_x = (imgBlur.at<unsigned char>(i, j+delta) - imgBlur.at<unsigned char>(i, j)) / delta;
-            gradient_y = (imgBlur.at<unsigned char>(i+delta, j) - imgBlur.at<unsigned char>(i, j)) / delta;
-            gradient = sqrt(gradient_x * gradient_x + gradient_y * gradient_y);
-            // theta = atan(gradient_x / gradient_y);
-            gradients[i][j] = gradient;
-            max_grad = max_grad < gradient ? gradient : max_grad;
-            min_grad = min_grad > gradient ? gradient : min_grad;
+            for (int j = 0; j < ncols-delta; j++)
+            {
+                gradient_x = (imgBlur.at<unsigned char>(i, j+delta) - imgBlur.at<unsigned char>(i, j)) / delta;
+                gradient_y = (imgBlur.at<unsigned char>(i+delta, j) - imgBlur.at<unsigned char>(i, j)) / delta;
+                gradient = sqrt(gradient_x * gradient_x + gradient_y * gradient_y);
+                // theta = atan(gradient_x / gradient_y);
+                gradients[i][j] = gradient;
+                max_grad = max_grad < gradient ? gradient : max_grad;
+                min_grad = min_grad > gradient ? gradient : min_grad;
+            }
+        }
+
+        for (int i = 0; i < nrows-delta; i++)
+        {
+            for (int j = 0; j < ncols-delta; j++)
+            {
+                norm_grad = (gradients[i][j] - min_grad)/(max_grad - min_grad);
+                magnitude.at<unsigned char>(i, j) = int(abs(norm_grad * 255));
+            }
         }
     }
-
-    cout << "max grad: " << max_grad << endl;
-    cout << "min grad: " << min_grad << endl;
-
-    for (int i = 0; i < nrows; i++)
+    else
     {
-        for (int j = 0; j < ncols; j++)
+        for (int i = 0; i < nrows; i++)
         {
-            norm_grad = (gradients[i][j] - min_grad)/(max_grad - min_grad);
-            magnitude.at<unsigned char>(i, j) = char(abs(norm_grad * 255));
+            for (int j = 0; j < ncols; j++)
+            {
+                magnitude.at<unsigned char>(i, j) = sqrt(pow(imgFilterX.at<unsigned char>(i, j), 2) + pow(imgFilterY.at<unsigned char>(i, j), 2));
+            }
         }
     }
 
